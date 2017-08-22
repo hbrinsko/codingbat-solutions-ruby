5dd909f59f7fc0c9572b8c253b9a6937f013c94c
Hannah Brinsko <h.brinsko@utexas.edu>
2017-08-22 11:35:01 -0500

    Logic-2 added

---
+++ Logic-2.rb
@@ -0,0 +1,68 @@
+def make_bricks(small, big, goal)
+    if goal > big * 5 + small
+        false
+    end
+    if goal % 5 >  small
+        false
+    end
+    true
+end
+
+def lone_sum(a,b,c)
+    if a==b && b==c
+        0
+    elsif a==b
+        c
+    elsif a==c
+        b
+    elsif b==c
+        a
+    else
+        a+b+c
+    end
+end
+
+def lucky_sum(a,b,c)
+    if a==13
+        0
+    elsif b ==13
+        a
+    elsif c == 13
+        a + b
+    else
+        a + b + c
+    end
+end
+
+def no_teen_sum(a,b,c)
+    sum = 0
+    arr = [a,b,c]
+    not_okay = [13,14,17,18,19]
+    arr.each {|num| sum += num unless not_okay.include?(num)}
+    sum
+end
+
+def round_10(num)
+    (num % 10 < 5) ? (num / 10 * 10)  : (num / 10 * 10 + 10)
+end
+
+def round_sum(a,b,c)
+    round_10(a) + round_10(b) + round_10(c)
+end
+
+def close_far(a,b,c)
+    if (b-c).abs < 2
+        false
+    end
+    (a-b).abs<=1 && (a-c).abs>=2 || (a-c).abs<=1 && (a-b).abs>=2
+end
+
+def make_chocolate(small, big, goal)
+    max_big = goal / 5
+    if big>max_big
+        goal <= 5 * max_big + small ? goal - 5 * max_big : -1
+    end
+    goal <= 5 * big + small ? goal - 5 * big : -1
+end
+    
+puts make_chocolate(4,1,7)
--- String-2.rb
+++ String-2.rb
@@ -3,30 +3,28 @@ def doublechar(str)
     answer = ""
     arr = str.split("")
     arr.each{|char| answer.concat(char).concat(char)}
-    return answer
+    answer
 end
 
 def count_hi(str)
-    return str.scan(/(?=hi)/).count
+    str.scan(/(?=hi)/).count
 end
 
 def catdog(str)
-    return str.scan(/(?=cat)/).count == str.scan(/(?=dog)/).count
+    str.scan(/(?=cat)/).count == str.scan(/(?=dog)/).count
 end
 
-def count_code(str)
-    return str.scan(/(?=co[a-z]e)/).count
-end
 
-#not working
-def end_other(str1, str2)
-    str1.downcase
-    str2.downcase
-    return str1.end_with?(str2) || str2.end_with?(str1)
+def end_other(a, b)
+    a.downcase
+    b.downcase
+    one_way = a.length >= b.length &&
+        a[(a.length - b.length)..-1].eql?(b)
+    other_way = b.length >= a.length &&
+        b[(b.length - a.length)..-1].eql?(a)
+    one_way || other_way
 end
 
 def xyz_there(str)
-    return str.scan(/([^\.]xyz)/).count >= 1
+    str.scan(/([^\.]xyz)/).count >= 1
 end
-
-puts xyz_there('hixyz')
